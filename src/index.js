5fb07293990cd5ca2da45311f168711f5ba8f997
olakouns <kounassolazare@gmail.com>
2023-12-10 14:04:45 +0000

    UPDATE

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const { faker } = require('@faker-js/faker');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const { faker } = require("@faker-js/faker");
 
-console.log(faker.date.between({
-    from: '2022-01-01', 
-    to: '2023-12-31'
-}));
+const ONE_SECOND_COST = 30; // Coût par seconde pour les appels
+const ONE_SMS_COST = 25; // Coût par SMS
+
+console.log(
+  faker.date.between({
+    from: "2022-01-01",
+    to: "2023-12-31",
+  })
+);
 
 // (806) 642-6947
 // 2023-12-07T04:15:32.809Z
@@ -14,80 +19,209 @@ console.log(faker.date.between({
 const app = express();
 const port = 3000;
 
-mongoose.connect("mongodb+srv://lazare:lazare@cluster0.xymug.mongodb.net/facturation_db").then((cnx) => {
+mongoose
+  .connect(
+    "mongodb+srv://lazare:lazare@cluster0.xymug.mongodb.net/facturation_db"
+  )
+  .then((cnx) => {
     console.log(`Database connected : ${cnx.connection.host}`);
   });
 
 // mongoose.connect('mongodb://localhost/cdr-api', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const cdrSchema = new mongoose.Schema({
-    callingNumber: String,  // Numéro de l'appelant
-    calledNumber: String,   // Numéro de l'appelé
-    imsi: String,
-    date: Date,
-    duration: Number,
-    type: {
-        type: String,
-        enum: ['PREPAID', 'POSTPAID'],
-    },
+  callingNumber: String, // Numéro de l'appelant
+  calledNumber: String, // Numéro de l'appelé
+  imsi: String,
+  date: Date,
+  duration: Number,
+  callType: {
+    type: String,
+    enum: ["CALL", "VOICE"],
+  },
+  type: {
+    type: String,
+    enum: ["PREPAID", "POSTPAID"],
+  },
 });
 
-const CDR = mongoose.model('CDR', cdrSchema);
+const CDR = mongoose.model("CDR", cdrSchema);
 
 app.use(bodyParser.json());
 
-app.get('/cdr/:phoneNumber/:startDate/:endDate', async (req, res) => {
-    const { phoneNumber, startDate, endDate } = req.params;
+app.post("/generate-invoices", async (req, res) => {
+  const phones = req.body;
 
-    try {
-        const cdrData = await CDR.find({
-            $and: [
-                { callingNumber: phoneNumber },
-                { date: { $gte: new Date(startDate), $lte: new Date(endDate) } },
-            ],
-        });
 
-        res.json(cdrData);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
+  try {
+    let result = [];
+    if (!Array.isArray(phones) || phones.length === 0) {
+      return res.status(400).json({
+        success: false,
+        message:
+          "Le corps de la requête doit contenir un tableau de phone number valides.",
+      });
     }
+  
+    for (const phone of phones) {  
+      // check if phone number exist
+      result.push(await generateInvoice(phone));
+    }
+
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+ 
 });
 
-app.post('/generate-cdr', async (req, res) => {
-    try {
-        const { phoneNumber, imsi, type } = req.body;
+app.get("/get-invoice/:phoneNumber", async (req, res) => {
+  const { phoneNumber } = req.params;
+  try {
+    res.json(await generateInvoice(phoneNumber));
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
-        // Générez et enregistrez 100 CDR
-        for (let i = 0; i < 100; i++) {
-            // Générez un CDR aléatoire avec une date aléatoire
-            const randomCDR = {
-                callingNumber: phoneNumber,
-                calledNumber: faker.phone.number(),
-                imsi: imsi,
-                date: faker.date.between({
-                    from: '2022-01-01', 
-                    to: '2023-12-31'
-                }),
-                duration: Math.floor(Math.random() * 600) + 1,
-                type: type,
-            };
+const generateInvoice = async (phoneNumber) => {
+  const currentDate = new Date();
+  const startOfMonth = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth(),
+    1
+  );
+  const endOfMonth = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth() + 1,
+    0
+  );
 
-            // Enregistrez le CDR dans la base de données
-            await CDR.create(randomCDR);
-        }
+  try {
+    const cdrData = await CDR.find({
+      $and: [
+        { callingNumber: phoneNumber },
+        { date: { $gte: startOfMonth, $lte: endOfMonth } },
+      ],
+    });
 
-        // Utilisez une structure d'API Response pour retourner une réponse formatée
-        res.json({
-            success: true,
-            message: 'Opération effectuée avec succès.'
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
+    // Calculer la somme des durations pour les appels et des SMS
+    const totalCallDuration = cdrData
+      .filter((cdr) => cdr.callType === "CALL")
+      .reduce((acc, cdr) => acc + cdr.duration, 0);
+
+    const totalSmsCount = cdrData
+      .filter((cdr) => cdr.callType === "VOICE") // Je suppose que 'VOICE' correspond aux SMS
+      .reduce((acc, cdr) => acc + cdr.duration, 0);
+
+    // Calculer les montants pour les appels et les SMS
+    const callAmount = totalCallDuration * ONE_SECOND_COST;
+    const smsAmount = totalSmsCount * ONE_SMS_COST;
+
+    // Calculer le montant total
+    const totalAmount = callAmount + smsAmount;
+
+    return {
+      phoneNumber: phoneNumber,
+      totalCallDuration: totalCallDuration,
+      totalSmsCount: totalSmsCount,
+      callAmount: callAmount,
+      smsAmount: smsAmount,
+      totalAmount: totalAmount,
+    };
+  } catch (error) {
+    throw error;
+  }
+};
+
+app.post("/save-cdr", async (req, res) => {
+  try {
+    const cdrs = req.body;
+
+    // Assurez-vous que l'objet 'cdrs' est un tableau d'objets CDR valides
+    if (!Array.isArray(cdrs) || cdrs.length === 0) {
+      return res.status(400).json({
+        success: false,
+        message:
+          "Le corps de la requête doit contenir un tableau d'objets CDR valides.",
+      });
     }
+
+    // Validez chaque objet CDR avant l'enregistrement
+    for (const cdr of cdrs) {
+      // Ajoutez vos validations ici
+      if (!isValidCdr(cdr)) {
+        return res.status(400).json({
+          success: false,
+          message: "Les objets CDR doivent avoir des propriétés valides.",
+        });
+      }
+    }
+
+    // Enregistrez les CDRs dans la base de données
+    await CDR.insertMany(cdrs);
+    // Utilisez une structure d'API Response pour retourner une réponse formatée
+    res.json({
+      success: true,
+      message: "Opération effectuée avec succès.",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Fonction de validation pour un objet CDR
+function isValidCdr(cdr) {
+  // Ajoutez vos validations spécifiques ici
+  return (
+    cdr &&
+    cdr.callingNumber &&
+    cdr.calledNumber &&
+    cdr.imsi &&
+    cdr.date &&
+    cdr.duration !== undefined &&
+    cdr.type &&
+    cdr.callType
+  );
+}
+
+app.post("/generate-cdr", async (req, res) => {
+  try {
+    const { phoneNumber, imsi, type } = req.body;
+
+    // Générez et enregistrez 100 CDR
+    for (let i = 0; i < 100; i++) {
+      // Générez un CDR aléatoire avec une date aléatoire
+      const randomCDR = {
+        callingNumber: phoneNumber,
+        calledNumber: faker.phone.number(),
+        imsi: imsi,
+        date: faker.date.between({
+          from: "2022-01-01",
+          to: "2023-12-31",
+        }),
+        duration: Math.floor(Math.random() * 600) + 1,
+        type: type,
+      };
+
+      // Enregistrez le CDR dans la base de données
+      await CDR.create(randomCDR);
+    }
+
+    // Utilisez une structure d'API Response pour retourner une réponse formatée
+    res.json({
+      success: true,
+      message: "Opération effectuée avec succès.",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
